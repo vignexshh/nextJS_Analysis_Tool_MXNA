7ef930745571f9060af3a8787a9c302f06573ed5
VigneshTD <vigneshtd111@gmail.com>
2024-11-22 23:11:48 +0530

    icons compitable with system theme preference

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link"
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
- 
 import { useState, useEffect } from 'react'
 import { Button } from "@/components/ui/button"
 import { ThemeProvider } from "@/components/theme-provider";
@@ -27,34 +26,43 @@ import {
 
 
 export default function LoginForm() {
+  const { theme, systemTheme } = useTheme();
+
+  // Ensure currentTheme is defined only after the theme is available
+  const currentTheme = theme === "system" ? systemTheme ?? "light" : theme ?? "light";
+
+
+  console.log("the theme is ", currentTheme);
   return (
-        
+
     <div className=" flex justify-center items-center bg-background w-full h-screen  ">
+      <div className="absolute flex justify-center gap-2 top-0 left-0 p-5 font-bold opacity-75 "> <img src="images/mh_logo.png" className="size-6 " style={{ filter: theme === "dark" ? "invert(1)" : "invert(0)", }} alt="Medicalhunt Logo" />  <h1> Medical Hunt </h1> </div>
       <div className="absolute top-0 right-0 p-5">
-      <ModeToggle />
+        <ModeToggle />
       </div>
-      <div className="w-96 m-5" > 
-      <Card>  
-        <CardHeader>
-        <div className="flex-row gap-0 pt-6 pl-6 pr-6 pb-4 "> 
-        <div className="size-16 bg-secondary rounded-full p-3 mb-3" > <img  src="images/mh_logo.png" alt="Medicalhunt Logo" />
-        </div>
-        
-          <CardTitle> Welcome to Medical Hunt </CardTitle>
-          <CardDescription> Continue Signin or signup </CardDescription>
-          
-          </div>
-          <div className="pt-5 border-t p-6">
-           {/* Fix button icon for dynamic dark / light mode  */}
-          <Button className="w-full font-semibold" > <div className="size-4">  <img  src="images/google-white-icon.webp" alt="Medicalhunt Logo" /> </div> Continue with Google</Button>
-          </div>
-          </CardHeader> </Card> 
-          </div>
+      <div className="w-96 m-5" >
+        <Card>
+          <CardHeader>
+            <div className="flex-row gap-0 pt-6 pl-6 pr-6 pb-4 ">
+              <div className="size-16 border rounded-full p-3 mb-3" > <img src="images/mh_logo.png" style={{ filter: theme === "dark" ? "invert(1)" : "invert(0)", }} alt="Medicalhunt Logo" />
+              </div>
 
 
-  
+              <CardTitle className="pb-2" > Welcome to Medical Hunt {currentTheme} </CardTitle>
+              <CardDescription> Continue Signin or signup </CardDescription>
+
+            </div>
+            <div className="pt-5 border-t p-5">
+              {/* Fix button icon for dynamic dark / light mode  */}
+              <Button className="w-full p-6 font-semibold" > <div className="size-4">  <img src={`images/google-icon-${currentTheme}.svg`} alt="google_icon_white" /> </div> Continue with Google</Button>
+            </div>
+          </CardHeader> </Card>
+      </div>
+
+
+
     </div>
- 
+
 
   )
 }
